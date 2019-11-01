f974f797d0bbb5e83d3e7bcfd968eea99fba749a
jjmr007 <money.as.flow@gmail.com>
2019-11-01 13:51:24 -0400

    Creating the contract for mutual credit system
    At the inception of mutual credit, negative balances were allowed. However it exposes the system to a Sybil attack. In this approach a collateralized debt position of certain asset is adopted.

---
+++ Mutual_Credit_Sys/Mutual_Credit_Main.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.12;
+
+contract Mutual {
+    
+    mapping (address => uint) public sAldos;
+    
+    function transfer(address a, uint pago) public returns (bool) {
+     
+     
+        
+    }
+    
+    
+    
+    
+}
