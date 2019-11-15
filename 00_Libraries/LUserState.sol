152ed1b90888792284da84e989fe18191d54451c
jjmr007 <money.as.flow@gmail.com>
2019-11-15 11:21:45 -0400

    User State library
    This library now serves to a permanent storage contract, regardless the upgrades.

--- 00_Libraries/LUserState.sol
+++ 00_Libraries/LUserState.sol
@@ -1,29 +1,37 @@
 pragma solidity ^0.5.12;
 
-import { ContractStt as CS } from "./LContractState.sol";
-
-import { SafeMath } from "./LSafeMath.sol";
-
 library UserStt {
     
-    using CS for CS.ContractState;
-    using SafeMath for uint256;
+    /*
+    *   it gives:
+    *   if an address is or not a member;
+    *   how many members there are;
+    *   a member address according an array position;
+    *   any balance of a member;
+    *
+    *   it allows:
+    *   add a member;
+    *   set any balance for a member address;
+    */
     
     struct UserState {
         
         bool    _member;
-        uint256 _BlockM;
-        uint256 _Debt;
-        uint256 _Merit;
-        uint256 _Pending;
-        uint256 _Credit;
-    // address[2]  _AlliedChains;
-
+        uint256[5]  _Balance;
+        /*
+        uint256 _BlockM;    // _Balance[0];
+        uint256 _Debt;      // _Balance[1];
+        uint256 _Merit;     // _Balance[2];
+        uint256 _Pending;   // _Balance[3];
+        uint256 _Credit;    // _Balance[4];
+        */
+        
     }
 
     struct  _us {
         
         mapping (address => UserState)  _userState;
+        address[]   _Members;
         
     }
     
@@ -33,87 +41,41 @@ library UserStt {
         
     }
     
-    function FreezedMerits(_us storage us, address a) internal view returns (uint256) {
+    function TotalMembers(_us storage us) internal view returns (uint256) {
         
-        return us._userState[a]._BlockM;
-        
-    }
-    
-    function InDebted(_us storage us, address a) internal view returns (uint256) {
-        
-        return us._userState[a]._Debt;
+        return us._Members.length;
         
     }
-    
-    function Merits(_us storage us, address a) internal view returns (uint256) {
-        
-        return us._userState[a]._Merit;
-        
-    }
-    
-    function Pendings(_us storage us, address a) internal view returns (uint256) {
-        
-        return us._userState[a]._Pending;
-        
-    }
-    
-    function Credits(_us storage us, address a) internal view returns (uint256) {
-        
-        return us._userState[a]._Credit;
-        
-    }
-    
-    function    ReceiveCredits(_us storage us, address _receiver, uint256 _payment) internal returns (bool) {
-        
-        if (us._userState[_receiver]._Debt > 0) {
-            if (us._userState[_receiver]._Debt <= _payment) { 
-            us._userState[_receiver]._Credit = _payment.sub(us._userState[_receiver]._Debt);
-            us._userState[_receiver]._Debt = 0 ; }   else { 
-            us._userState[_receiver]._Debt = us._userState[_receiver]._Debt.sub(_payment);    }
-        }   else {
-                us._userState[_receiver]._Credit = us._userState[_receiver]._Credit.add(_payment);
-            }
-        
-        return true;
-        
-    }
-    
-    function    SendCredits(_us storage us, CS.ContractState storage cs, address _sender, address _receiver, uint256 _payment) 
-    internal returns (bool) {
-        
-        require (us._userState[_receiver]._member && us._userState[_sender]._member);
-        if (us._userState[_sender]._Credit < _payment) {
-            
-            require (us._userState[_sender]._BlockM.mul(cs._X[0]) > _payment.mul(100).div(cs._X[1])); 
-            ReceiveCredits(us, _receiver, _payment);
-            uint256 Delta = _payment.sub(us._userState[_sender]._Credit) ;
-            us._userState[_sender]._Debt = Delta;
-            us._userState[_sender]._Credit = 0;
-            
-        }   else    { us._userState[_sender]._Credit = us._userState[_sender]._Credit.sub(_payment);
-                        ReceiveCredits(us, _receiver, _payment);    }
-        
-    }
-    
-    function    ReceiveMerits(_us storage us, address _receiver, uint256 _payment) internal returns (bool) {
-        
-        
-    }
-    
-    function    ReceivePendings(_us storage us, address _receiver, uint256 _payment) internal returns (bool) {
-        
-        
-    }
-    
-    /*
-    function SetAlly(_us storage us, addres a) public returns(bool) {
-        require (_userState[msg.sender]._member);
-        us._userState[msg.sender]._AlliedChains[1] = us._userState[msg.sender]._AlliedChains[0]; 
-        us._userState[msg.sender]._AlliedChains[0] = a;
-    }
-    */
 
+    function GetMember(_us storage us, uint256 k) internal view returns (address) {
+        
+        require(us._Members.length > k, "that member number does not exist");
+        return us._Members[k];
+        
+    }
     
+    function GetBalances(_us storage us, address a, uint8 b) internal view returns (uint256) {
+        
+        require (b < 5, "Not a valid register");
+        require(us._userState[a]._member, "Not a valid member");
+        return us._userState[a]._Balance[b];
+        
+    }
     
+    function SetMember(_us storage us, address a) internal {
+        
+        require(!us._userState[a]._member);
+        us._Members.push(a);
+        us._userState[a]._member = true;
+        
+    }
     
+    function SetBalance(_us storage us, address a, uint8 b, uint256 _balance) internal {
+        
+        require (b < 5, "Not a valid register");
+        require(us._userState[a]._member, "Not a valid member");
+        us._userState[a]._Balance[b] = _balance;
+        
+    }
+
 }
