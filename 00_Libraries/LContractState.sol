11aad9bee0da3d46a123f028729861109647e287
jjmr007 <money.as.flow@gmail.com>
2019-11-15 11:15:55 -0400

    Contract State library
    Now this library serves to a permanent storage; the same contract regardless the upgrades

--- 00_Libraries/LContractState.sol
+++ 00_Libraries/LContractState.sol
@@ -2,131 +2,77 @@ pragma solidity ^0.5.12;
 
 library ContractStt {
     
+        /*
+    *   it gives:
+    *   any total balance of the contract;
+    *   any merit parameter according to an index (Value in credits, % of collateral, minimum quorum);
+    *   if an address contract is an allied master store contract;
+    *   an allied address by the array index;
+    *   how many alliances there are;
+    *
+    *   it allows:
+    *   set any total balance by index;
+    *   set the whole array of merit parameters;
+    *   set a new alliance (bridge);
+    */
+    
     struct ContractState {
-        
-    uint256 _Reference;                                             // 1.-accessible from helpings
-    uint256 _TotalMembers;                                          // 2.-view from helpings
-    uint256 _BlocdMrts;                                             // 3.-view from Merits
-    uint256 _TotalDebt;                 // view from credits
-    uint256 _FreeMerits;                // erc20 Merits; balance of
-    uint256 _TotalPendings;             // erc20 Pendings; balance of
-    uint256 _TotalCredits;              // erc20 Credits; balance of
+    
+    uint256[6]  _Sums;
+    /*    
+    uint256 _Reference;
+    uint256 _BlocdMrts;
+    uint256 _TotalDebt;
+    uint256 _FreeMerits
+    uint256 _TotalPendings;
+    uint256 _TotalCredits;
+    */
     
     uint256[3]  _X; // _X[0] is value of merits in credits, _X[1] is % valid collateral, _X[2] is minimum quorum (Governance)
-    address[]   _Members;                                           // 4.-view from helpings
-    mapping(address => bool) _Bridges;                              // 5.-view from helpings
-    address[]   _BRdgs; // in case of migration a register of bridges is allways necessary
+    mapping(address => bool) _Bridges;
+    address[]   _BRdgs; 
     
-    address _Principal;                 
-    address _ERC20_Merits;                                          // 6.-view from helpings
-    address _ERC20_Pendings;                                        // 7.-view from helpings
-    address _ERC20_Credits;                                         // 8.-view from helpings
-    address _Govern;                                                // 9.-view from helpings
-    address _Migrate;   // if this address is NOT zero (0x00...00)    10.-view from helpings
-                        //is both, a flag and the destination of the new contract version
-        
     }
 
-    function CreationTime(ContractState storage cs) internal view returns(uint256) {
+    function GetTotals(ContractState storage cs, uint8 b) internal view returns(uint256) {
         
-        return cs._Reference;
+        require (b < 6, "Not a valid register");
+        return cs._Sums[b];
         
     }
     
-    function Members(ContractState storage cs) internal view returns(uint256) {
+    function SetNetValue(ContractState storage cs, uint8 b, uint256 newTotal) internal {
         
-        return cs._TotalMembers;
-        
-    }
-
-    function FreezedM(ContractState storage cs) internal view returns(uint256) {
-        
-        return cs._BlocdMrts;
+        require ((b < 6 && b != 0), "Not a valid register");
+        cs._Sums[b] = newTotal;
         
     }
 
-    function NetDebt(ContractState storage cs) internal view returns(uint256) {
+    function GetMeritValues(ContractState storage cs, uint8 i) internal view returns(uint256) {
         
-        return cs._TotalDebt;
-        
-    }
-
-    function NetMerits(ContractState storage cs) internal view returns(uint256) {
-        
-        return cs._FreeMerits;
-        
-    }
-
-    function NetPendings(ContractState storage cs) internal view returns(uint256) {
-        
-        return cs._TotalPendings;
-        
-    }
-
-    function NetCredits(ContractState storage cs) internal view returns(uint256) {
-        
-        return cs._TotalCredits;
-        
-    }
-
-    function MeritValues(ContractState storage cs, uint8 i) internal view returns(uint256) {
-        
+        require (i < 3, "Not a valid register");
         return cs._X[i];
         
     }
-
-    function MemberbyID(ContractState storage cs, uint256 i) internal view returns(address) {
+    
+    function SetMeritValues(ContractState storage cs, uint256[3] memory X) internal {
         
-        return cs._Members[i];
-        
-    }
-
-    function isBridge(ContractState storage cs, address a) internal view returns(bool) {
-        
-        return cs._Bridges[a];
+        cs._X = X;
         
     }
 
     function BridgebyID(ContractState storage cs, uint256 i) internal view returns(address) {
         
+        require(cs._BRdgs.length > i, "that bridge number does not exist");
         return cs._BRdgs[i];
         
     }
-
-    function Home(ContractState storage cs) internal view returns(address) {
+    
+    function SetBridge(ContractState storage cs, address a) internal {
         
-        return cs._Principal;
-        
-    }
-
-    function MeritERC20(ContractState storage cs) internal view returns(address) {
-        
-        return cs._ERC20_Merits;
-        
-    }
-
-    function PendERC20(ContractState storage cs) internal view returns(address) {
-        
-        return cs._ERC20_Pendings;
-        
-    }
-
-    function CreditERC20(ContractState storage cs) internal view returns(address) {
-        
-        return cs._ERC20_Credits;
-        
-    }
-
-    function GovernAddr(ContractState storage cs) internal view returns(address) {
-        
-        return cs._Govern;
-        
-    }
-
-    function MigratingTo(ContractState storage cs) internal view returns(address) {
-        
-        require (cs._Migrate != address(0), "Not in a Migration Process");
-        return cs._Migrate;
+        require(!cs._Bridges[a]);
+        cs._Bridges[a] = true;
+        cs._BRdgs.push(a);
         
     }
 
